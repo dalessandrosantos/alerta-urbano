7309a66e75a1729b75a518c40b6e6c67ced02cbd
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-16 23:02:12 -0300

    estiliza página login

--- ocorrencias/forms.py
+++ ocorrencias/forms.py
@@ -6,8 +6,21 @@ class OcorrenciaForm(forms.ModelForm):
         model = Ocorrencia
         fields = ['categoria', 'descricao', 'localizacao', 'imagem']
 
+        # Define tamanho do campo "Descriçao"
+        widgets = {
+            'descricao': forms.Textarea(attrs={
+                'rows': 4,
+            }),
+        }
+
 # usado em: editar (painel)
 class OcorrenciaEditForm(forms.ModelForm):
     class Meta:
         model = Ocorrencia
         fields = ['categoria', 'descricao', 'localizacao', 'imagem', 'status']
+
+        widgets = {
+            'descricao': forms.Textarea(attrs={
+                'rows': 4,
+            }),
+        }
